1b8899a59b6e0dcdce4264c8c684d7051471c025
ArcasHH <asyaludv@mail.ru>
2026-05-01 18:35:29 +0300

    base structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ArcasHH.github.io',
+    //base: '/',
+    base: '/portfolio/',
+    trailingSlash: 'always',
+    vite: {
+        plugins: [tailwindcss()]
+    }
+});
