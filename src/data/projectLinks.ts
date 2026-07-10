5ebf7ae9364bced0b4aca0636c57d2cd3ea4742f
ArcasHH <asyaludv@mail.ru>
2026-07-10 19:35:31 +0300

    Update projectLinks.ts

--- src/data/projectLinks.ts
+++ src/data/projectLinks.ts
@@ -11,7 +11,7 @@ export interface ProjectLinks {
 
 export const projectLinks: ProjectLinks[] = [
     {
-        slug: 'aceage',
+        slug: 'uhoh',
         //github: 'https://github.com/PureshkaGames/AceAge',
         //itch: 'https://clinicallysleepy.itch.io/ace-age',
         myindie: 'https://myindie.ru/games/game/ne-ssy_6v1',
